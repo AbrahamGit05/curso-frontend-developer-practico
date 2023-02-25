ef79d039c262e3cb73cd1c93ed5d7e5c8d4c12d9
AbrahamGit05 <pepeken2020@gmail.com>
2023-02-25 15:30:30 -0500

    clase 3 compatibilidad

--- main.js
+++ main.js
@@ -4,15 +4,45 @@ document.querySelector(selector);
 const menuEmail = s_('.navbar-email');
 const desktopMenu = s_('.desktop-menu');
 const HamburguerIcon = s_('.menu');
+const menuCarritoIcon = s_('.navbar-shopping-cart');
 const mobileMenu= s_('.mobile-menu');
+const aside = s_('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 HamburguerIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
+
     mobileMenu.classList.toggle('inactive');
 }
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+if (!isMobileMenuClosed) {
+            mobileMenu.classList.add('inactive')
+        }
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive')
+    }
+        aside.classList.toggle('inactive')
+    }
